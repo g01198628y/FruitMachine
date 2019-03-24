bb564206ff2a24fc10956e7e99f05ed5200cb876
gary.lai99 <gary.lai99@TI03NB133>
2019-03-24 15:39:46 +0800

    Add TestMethod "FruitMachine_Three_Cherry_Should_Return_50".

--- FruitMachine/FruitMachineResultTest.cs
+++ FruitMachine/FruitMachineResultTest.cs
@@ -27,5 +27,15 @@ namespace FruitMachine
             int[] spins = new int[] { 9, 9, 9 };
             Assert.AreEqual(10, score.ShowResult(reels, spins));
         }
+
+        [TestMethod]
+        public void FruitMachine_Three_Cherry_Should_Return_50()
+        {
+            var score = new CalScore();
+            string[] reel = new string[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = new int[] { 5, 5, 5 };
+            Assert.AreEqual(50, score.ShowResult(reels, spins));
+        }
     }
 }
