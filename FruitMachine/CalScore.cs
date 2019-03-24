0a0cdb877995ce3e1e37036b06760ca8a0770a1e
gary.lai99 <gary.lai99@TI03NB133>
2019-03-24 14:22:22 +0800

    Add TestMethod "FruitMachine_Three_Wild_Should_Return_100"

---
+++ FruitMachine/CalScore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitMachine
+{
+    public class CalScore
+    {
+        public int ShowResult(List<string[]> reels, int[] spins)
+        {
+            var reelOneItem = reels[0][spins[0]];
+            var reelTwoItem = reels[1][spins[1]];
+            var reelThreeItem = reels[2][spins[2]];
+
+            if (reelOneItem == "Wild" && reelTwoItem == "Wild" && reelThreeItem == "Wild")
+            {
+                return 100;
+            }
+            return 0;
+        }
+    }
+}
---
+++ FruitMachine/FruitMachineResultTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FruitMachine
+{
+    [TestClass]
+    public class FruitMachineResultTest
+    {
+        [TestMethod]
+        public void FruitMachine_Three_Wild_Should_Return_100()
+        {
+            var score = new CalScore();
+            string[] reel = new string[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+            int[] spins = new int[] { 0, 0, 0 };
+            Assert.AreEqual(100, score.ShowResult(reels, spins));
+        }
+    }
+}
