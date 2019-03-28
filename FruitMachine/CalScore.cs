107f668b77857604d357f8b6936b7f144c7edb48
gary.lai99 <gary.lai99@TI03NB133>
2019-03-28 23:24:43 +0800

    Refactor switch case to dictionary.

--- FruitMachine/CalScore.cs
+++ FruitMachine/CalScore.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace FruitMachine
 {
@@ -19,41 +15,29 @@ namespace FruitMachine
         {
             var wildRepeatNum = GetWildRepeatNum(reelResult);
             var repeatItemInfo = GetRepeatItemInfo(reelResult);
+            return repeatItemInfo == null ? 0 : GetScore(repeatItemInfo, wildRepeatNum);
+        }
 
-            if (repeatItemInfo != null)
-            {
-                switch (repeatItemInfo.Amount)
-                {
-                    case 3:
-                        return GetScore(repeatItemInfo.Name, Bonus.ThreeOfSame);
-
-                    case 2:
-                        return _itemScoringLookUp[repeatItemInfo.Name] * _twoOfSameBonusLookUp[wildRepeatNum];
-                }
-            }
-            return 0;
+        private int GetScore(Item repeatItemInfo, int wildRepeatNum)
+        {
+            return _itemScoringLookUp[repeatItemInfo.Name] * _repeatItemBonus[repeatItemInfo.Amount] * _wildBonusLookUp[wildRepeatNum];
         }
 
         private Item GetRepeatItemInfo(List<string> reelResult)
         {
-            if (hasRepeatItem(reelResult))
+            if (HasRepeatItem(reelResult))
             {
                 return reelResult.GroupBy(x => x)
                                .Where(x => x.Count() > 1)
-                               .OrderBy(x => x.Key)
+                               .OrderByDescending(x => x.Key)
                                .Select(x => new Item { Name = x.Key, Amount = x.Count() }).First();
             }
             return null;
         }
 
-        private bool hasRepeatItem(List<string> reelResult)
+        private bool HasRepeatItem(List<string> reelResult)
         {
             return reelResult.Count != reelResult.Distinct().ToList().Count;
-        }
-
-        private int GetScore(string itemName, Bonus bonus)
-        {
-            return _itemScoringLookUp[itemName] * (int)bonus;
         }
 
         private int GetWildRepeatNum(List<string> reelResult)
@@ -63,36 +47,27 @@ namespace FruitMachine
 
         private readonly Dictionary<string, int> _itemScoringLookUp = new Dictionary<string, int>()
         {
-            { "Wild",10 },
-            { "Star",9},
-            { "Bell",8},
-            { "Shell",7},
-            { "Seven",6},
-            { "Cherry",5},
-            { "Bar",4},
-            { "King",3},
-            { "Queen",2},
-            { "Jack",1}
+            { "Wild", 10 },
+            { "Star", 9},
+            { "Bell", 8},
+            { "Shell", 7},
+            { "Seven", 6},
+            { "Cherry", 5},
+            { "Bar", 4},
+            { "King", 3},
+            { "Queen", 2},
+            { "Jack", 1}
         };
 
-        private readonly Dictionary<int, int> _twoOfSameBonusLookUp = new Dictionary<int, int>()
+        private readonly Dictionary<int, int> _wildBonusLookUp = new Dictionary<int, int>()
         {
-            {0,1},{1,2},{2,1}
+            {0,1},{1,2},{2,1},{3,1}
         };
 
-        private enum GroupInfo
+        private readonly Dictionary<int, int> _repeatItemBonus = new Dictionary<int, int>()
         {
-            ThreeOfSame = 3,
-            TwoOfSame = 2,
-        }
-
-        private enum Bonus
-        {
-            TwoWildOneOther = 1,
-            TwoOfSameNoWild = 1,
-            TwoOfSameOneWild = 2,
-            ThreeOfSame = 10
-        }
+            {3,10},{2,1}
+        };
     }
 
     public class Item
